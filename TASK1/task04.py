a27c55bbbc0771420f0775ac5338be0995d69eef
JacyF <brunoloouuiis@gmail.com>
2023-02-01 21:57:32 +0300

    adeed task04

--- TASK1/task04.py
+++ TASK1/task04.py
@@ -4,3 +4,12 @@
 # (то есть разломить шоколадку на два прямоугольника).
 # 3 2 4 -> yes
 # 3 2 1 -> no
+
+n = int(input("Introduce value of n : "))
+m = int(input("Introduce value of m : "))
+k = int(input("Introduce value of k : "))
+
+if ((k < n * m ) and (k % n == 0 or k % m == 0)):
+    print("Yes")
+else:
+    print("No")
